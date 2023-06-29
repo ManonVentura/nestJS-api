ca97523ea02d400dc714cb6aa7de903aa07cf56e
ManonVentura <m.ventura@vatel.fr>
2023-06-29 10:14:56 +0200

    end of configuration with prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { TripsModule } from './trips/trips.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, TripsModule],
+  imports: [AuthModule, UserModule, BookmarkModule, TripsModule, PrismaModule],
 })
 export class AppModule {}
